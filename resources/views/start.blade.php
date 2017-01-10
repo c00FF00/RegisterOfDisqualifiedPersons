cf24702e344c7e2894b672bd843c1e945a7d16a8
gig <grig@esso.ru>
2017-01-10 10:08:53 +0300

    Страница индекс.

--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -1,2 +1,44 @@
-<?php
-echo 'СТРАНИЦА СТАРТ';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+</head>
+<body>
+
+
+<div class="page-header">
+    <h2>Реестр <small>дискфалифицированных лиц</small></h2>
+</div>
+
+<div class="input-group">
+    <span class="input-group-addon" id="sizing-addon2">@</span>
+    <input type="text" class="form-control" placeholder="Username" aria-describedby="sizing-addon2">
+</div>
+
+
+<div class="panel panel-default">
+    <div class="panel-heading">
+        <h3 class="panel-title">Panel title</h3>
+    </div>
+    <div class="panel-body">
+        Panel content
+    </div>
+</div>
+
+<button type="button" class="btn btn-success">Найти</button>
+
+
+
+</body>
+</html>
+
